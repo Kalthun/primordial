5cf8bc75f2382af1cca9da0b730f5175eba2b483
James Kalthun McDonagh <jameskwm@protonmail.com>
2025-01-27 12:59:35 -0500

    pray

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "NixOS config flake";
+
+  inputs = {
+
+    #
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    #
+    
+    
+  };
+
+  outputs = { self, nixpkgs }: {
+
+  };
+}
