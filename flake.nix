63e13e81860996637a2b1ed8c453ef2cb98dacc1
James Kalthun McDonagh <jameskwm@protonmail.com>
2025-01-27 17:55:00 -0500

    FLAKE FLAKE FLAKE

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+    description = "NixOS Configuration Flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    outputs = {
+        self, nixpkgs, ...
+    } @inputs: {
+        nixosConfigurations = {
+            seed = nixpkgs.lib.nixosSystem {
+                specialArgs = { inherit inputs; };
+                modules = [
+                    ./configuration.nix
+                ];
+            };
+        };
+    };
+}
