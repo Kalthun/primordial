2038b16597196eaff004b111f64f533947633b95
James Kalthun McDonagh <jameskwm@protonmail.com>
2025-01-29 20:15:14 -0500

    fixed typo

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-        nixos-hardware.url = "github.NixOS/nixos-hardware/master";
+        nixos-hardware.url = "github:nixos/nixos-hardware/master";
 
         home-manager = {
             url = "github:nix-community/home-manager";
