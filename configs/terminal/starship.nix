c6ac48596a6daf43ea0245f749187678afea3b23
James Kalthun McDonagh <jameskwm@protonmail.com>
2025-02-02 17:33:30 -0500

    I'll do it later man.

--- configs/terminal/starship.nix
+++ configs/terminal/starship.nix
@@ -4,35 +4,7 @@
 
     enable = true;
 
-    settings = {
-
-      format = lib.concatStrings [
-
-        "[╭─](bold green)"
-        "[\\(](bold white)"
-        "$all"
-        "${custom.end}"
-        "[\\)](bold white)"
-        "$cmd_duration"
-        "$linebreak"
-        "[╰─](bold green)"
-        "$jobs"
-        "$battery"
-        "$time"
-        "$status"
-        "$os"
-        "$container"
-        "$shell"
-        "$character"
-      ];
-
-      custom_end = {
-        format = " ";
-        style = "bold white";
-        when = "[ 'a' = 'a' ]";
-      };
-
-    };
+    settings = pkgs.lib.importTOML ./starship.toml;
 
   };
 
