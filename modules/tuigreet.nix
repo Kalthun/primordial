fce112e72ba6a19cca26ea8bb506262894e28901
James Kalthun McDonagh <jameskwm@protonmail.com>
2025-02-02 14:54:59 -0500

    dumb. I am dumb.

--- modules/tuigreet.nix
+++ modules/tuigreet.nix
@@ -5,7 +5,7 @@
     settings = {
       default_session = {
         command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --asterisks --cmd Hyprland";
-        user = kalthun;
+        user = "kalthun";
       };
     };
   };
