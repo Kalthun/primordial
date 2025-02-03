4609df6682bcb507a9e8ad5fb8ea6f56826cf8d5
James Kalthun McDonagh <jameskwm@protonmail.com>
2025-02-03 09:32:12 -0500

    fixing sound

--- hosts/seed/configuration.nix
+++ hosts/seed/configuration.nix
@@ -55,11 +55,22 @@
 
   programs.thunar.enable = true;
 
+  # Enable sound with pipewire.
+  # sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+  
   # Need this for zsh to work
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
-
-
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -70,6 +81,7 @@
   wofi
   hyprpaper
 
+  pavucontrol
   vscode
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
