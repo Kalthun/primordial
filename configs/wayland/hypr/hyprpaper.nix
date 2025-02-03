5c42c786d7a5be4977de2a36c638ddc7319dca26
kalthun <jameskwm@protonmail.com>
2025-02-03 17:12:25 -0500

    lets try this out

--- configs/wayland/hypr/hyprpaper.nix
+++ configs/wayland/hypr/hyprpaper.nix
@@ -1,4 +1,16 @@
-{
+{ config, pkgs, ... }: {
 
+  services.hyprpaper = {
+    enable = true;
+
+    settings = {
+      preload = [
+        "../../../wallpapers/art.png"
+      ];
+      wallpaper = [
+        ", ../../../wallpapers/art.png"
+      ];
+    };
+  };
 }
 # TODO
