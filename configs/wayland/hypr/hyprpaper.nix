269be55e72d44607e702ed22bf3a17e3fffe5bf6
kalthun <jameskwm@protonmail.com>
2025-02-03 18:03:58 -0500

    idk maybe this helps

--- configs/wayland/hypr/hyprpaper.nix
+++ configs/wayland/hypr/hyprpaper.nix
@@ -1,14 +1,19 @@
 { config, pkgs, ... }: {
+
   services.hyprpaper = {
+
     enable = true;
+
     settings = {
       preload = [
         "../../../wallpapers/art.png"
       ];
+
       wallpaper = [
         "DP-2,../../../wallpapers/art.png"
       ];
     };
+
   };
+
 }
-# TODO
