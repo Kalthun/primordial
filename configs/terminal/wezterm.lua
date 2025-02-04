9d30fed014175450143dc9a2934a091c9d664e41
kalthun <jameskwm@protonmail.com>
2025-02-04 12:07:05 -0500

    let's run this bad boi

--- configs/terminal/wezterm.lua
+++ configs/terminal/wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font 'Caskaydia Cove Nerd Font Mono'
+config.font = wezterm.font 'Caskaydia Cove Nerd Font Propo'
+
+config.window_close_confirmation = "NeverPrompt"
+config.enable_tab_bar = "false"
 
 return config
