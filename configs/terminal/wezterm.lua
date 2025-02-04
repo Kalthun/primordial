6a274309de8d9596dfbb04fa254c9e2c85305f7a
kalthun <jameskwm@protonmail.com>
2025-02-04 13:52:59 -0500

    last change for now

--- configs/terminal/wezterm.lua
+++ configs/terminal/wezterm.lua
@@ -6,8 +6,8 @@ config.enable_tab_bar = false
 config.default_cursor_style = 'SteadyBar'
 
 config.window_padding = {
-  left = 2,
-  right = 2,
+  left = 1,
+  right = 0,
   top = 0,
   bottom = 0,
 }
