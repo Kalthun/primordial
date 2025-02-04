1aa77335bb3cd5e7058f46d1226999cf61a0352b
kalthun <jameskwm@protonmail.com>
2025-02-04 15:17:25 -0500

    fixed color issues with tmux

--- configs/terminal/tmux.nix
+++ configs/terminal/tmux.nix
@@ -4,6 +4,12 @@
 
     enable = true;
 
+    extraConfig = ''
+      set-option -ga terminal-overrides ",xterm-256color:Tc"
+    # '';
+
+    terminal = "wezterm";
+
     plugins = with pkgs; [
       tmuxPlugins.sensible
     ];
