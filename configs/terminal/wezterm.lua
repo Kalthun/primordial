2f2b960f781052ed603d65a045fbd41e0a148a30
kalthun <jameskwm@protonmail.com>
2025-02-04 13:50:01 -0500

    padding

--- configs/terminal/wezterm.lua
+++ configs/terminal/wezterm.lua
@@ -5,4 +5,11 @@ config.window_close_confirmation = "NeverPrompt"
 config.enable_tab_bar = false
 config.default_cursor_style = 'SteadyBar'
 
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+
 return config
