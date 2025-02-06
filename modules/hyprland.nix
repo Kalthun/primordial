9428af294a127453d23fbb54ca3e9c7d5052b891
kalthun <jameskwm@protonmail.com>
2025-02-06 00:56:19 -0500

    trying something new

---
+++ modules/hyprland.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }: {
+
+  programs.hyprland.enable = true;
+  # programs.hyprland.xwayland.enable = true;
+  
+  environment = {
+    sessionVariables = {
+
+      WLR_NO_HARDWARE_CURSORS = "1";
+      
+    };
+  };
+}
