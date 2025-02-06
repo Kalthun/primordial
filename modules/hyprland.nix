ad1a2e98a21fbe2b39587563ea368537ffb1682f
kalthun <jameskwm@protonmail.com>
2025-02-06 17:01:55 -0500

    idk hoping this doesn't keep breaking

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }: {
 
   programs.hyprland.enable = true;
-  programs.hyprland.xwayland.enable = true;
+  # programs.hyprland.xwayland.enable = true;
   
   environment = {
     sessionVariables = {
