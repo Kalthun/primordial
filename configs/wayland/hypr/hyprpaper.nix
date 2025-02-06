8bf28799c44e7b29ea2b2989bab73e48158ae258
kalthun <jameskwm@protonmail.com>
2025-02-05 23:19:35 -0500

    why did these not get added??

--- configs/wayland/hypr/hyprpaper.nix
+++ configs/wayland/hypr/hyprpaper.nix
@@ -8,6 +8,7 @@
 
       preload = [
         "~/primordial/wallpapers/art.png"
+        "~/primordial/wallpapers/sg.png"
         "~/primordial/wallpapers/knight.png"
       ];
 
@@ -15,6 +16,8 @@
         "DP-1,~/primordial/wallpapers/art.png"
         "DP-2,~/primordial/wallpapers/art.png"
         "eDP-1,~/primordial/wallpapers/knight.png"
+        "DP-6,~/primordial/wallpapers/sg.png"
+        "DP-7,~/primordial/wallpapers/art.png"
       ];
 
     };
