2cfc4e86d071b4875aa023320700a0f8f5ae75c6
kalthun <jameskwm@protonmail.com>
2025-02-07 17:21:30 -0500

    adding wallpaps

--- configs/wayland/hypr/hyprpaper.nix
+++ configs/wayland/hypr/hyprpaper.nix
@@ -10,12 +10,14 @@
         "~/primordial/wallpapers/art.png"
         "~/primordial/wallpapers/sg.png"
         "~/primordial/wallpapers/knight.png"
+
+        "~/primordial/wallpapers/fre/fre-aftermath.png"
+        "~/primordial/wallpapers/fre/fre-promises.png"
       ];
 
       wallpaper = [
-        "DP-1, ~/primordial/wallpapers/art.png"
-        "DP-2, ~/primordial/wallpapers/art.png"
-        "eDP-1,~/primordial/wallpapers/knight.png"
+        "DP-1, ~/primordial/wallpapers/fre/fre-aftermath.png"
+        "eDP-1,~/primordial/wallpapers/fre/fre-aftermath.png"
         "DP-6, ~/primordial/wallpapers/sg.png"
         "DP-7, ~/primordial/wallpapers/art.png"
       ];
