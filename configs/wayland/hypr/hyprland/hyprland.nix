21246fafc245911d32ef2533a6306c57cce791af
kalthun <jameskwm@protonmail.com>
2025-02-07 20:12:53 -0500

    random

--- configs/wayland/hypr/hyprland/hyprland.nix
+++ configs/wayland/hypr/hyprland/hyprland.nix
@@ -8,7 +8,6 @@
       exec-once = [
         # "hyprctl setcursor catppuccin-mocha-light-cursors 48"
         # "hyprctl setcursor Pokemon 36"
-        # "hyprctl setcursor Gruppled Black 36"
       ];
       
     };
