0cc102d8943080b94670e068f197414cb76764df
kalthun <jameskwm@protonmail.com>
2025-02-07 20:42:18 -0500

    BIGGER

--- configs/stylix.nix
+++ configs/stylix.nix
@@ -53,7 +53,7 @@
     transparentBackground.signColumn = true;
   };
 
-  wayland.windowManager.hyprland.settings.general."col.active_border" = lib.mkForce "rgb(${toString config.lib.stylix.colors.base02}) rgb(${config.lib.stylix.colors.base0A}) 45deg";
-  wayland.windowManager.hyprland.settings.general."col.inactive_border" = lib.mkForce "rgb(${toString config.lib.stylix.colors.base04})";
+  wayland.windowManager.hyprland.settings.general."col.active_border" = lib.mkForce "rgba(${toString config.lib.stylix.colors.base0D}ee) rgba(${config.lib.stylix.colors.base0A}ee) 45deg";
+  wayland.windowManager.hyprland.settings.general."col.inactive_border" = lib.mkForce "rgba(${toString config.lib.stylix.colors.base04}aa)";
 
 }
