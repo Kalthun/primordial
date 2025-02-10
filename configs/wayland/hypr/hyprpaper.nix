7f2bfe83dceef760e913bc9e872e3ed3b27f10b0
kalthun <jameskwm@protonmail.com>
2025-02-09 21:13:18 -0500

    just one for now

--- configs/wayland/hypr/hyprpaper.nix
+++ configs/wayland/hypr/hyprpaper.nix
@@ -7,17 +7,17 @@
     settings = {
 
       preload = [
-        "~/primordial/wallpapers/art.png"
-        "~/primordial/wallpapers/sg.png"
-        "~/primordial/wallpapers/knight.png"
+        # "~/primordial/wallpapers/art.png"
+        # "~/primordial/wallpapers/sg.png"
+        # "~/primordial/wallpapers/knight.png"
 
         "~/primordial/wallpapers/fre/fre-aftermath.png"
-        "~/primordial/wallpapers/fre/fre-promises.png"
+        # "~/primordial/wallpapers/fre/fre-promises.png"
       ];
 
       wallpaper = [
         "DP-3, ~/primordial/wallpapers/fre/fre-aftermath.png"
-        "eDP-1,~/primordial/wallpapers/art.png"
+        "eDP-1,~/primordial/wallpapers/fre/fre-aftermath.png"
         "DP-6, ~/primordial/wallpapers/fre/fre-aftermath.png"
         "DP-7, ~/primordial/wallpapers/fre/fre-aftermath.png"
       ];
