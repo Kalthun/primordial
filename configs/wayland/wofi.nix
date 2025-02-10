5f712752a3893a754ffc21c0d0e07416c0a33d69
kalthun <jameskwm@protonmail.com>
2025-02-10 09:15:22 -0500

    using fuzzel and normal mode in starship

--- configs/wayland/wofi.nix
+++ configs/wayland/wofi.nix
@@ -1,11 +1,5 @@
 { config, pkgs, lib, ... }: {
-
-  programs.wofi.enable = true;
-
-  programs.tofi.enable = true;
-
-  programs.bemenu.enable = true;
-  
+ 
   programs.fuzzel.enable = true;
   
 }
