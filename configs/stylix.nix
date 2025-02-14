8790702153cd5442eb82251be88b296781c174e6
kalthun <jameskwm@protonmail.com>
2025-02-14 14:03:27 -0500

    pray

--- configs/stylix.nix
+++ configs/stylix.nix
@@ -1,66 +1,74 @@
 { config, pkgs, lib, ... }: {
 
-  # stylix.image = ../wallpapers/kanagawa.png;
+  stylix = {
 
-  stylix.enable = true;
-  stylix.autoEnable = true;
+    # image = ../wallpapers/kanagawa.png;
 
-  stylix.opacity.terminal = 0.85;
-  stylix.opacity.desktop = 0.0;
-  stylix.opacity.applications = 0.85;
-  stylix.opacity.popups = 0.85;
+    enable = true;
+    autoEnable = true;
 
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
-
-  stylix.fonts = {
-    serif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Serif";
+    opacity = {
+      terminal = 0.85;
+      desktop = 0.0;
+      applications = 0.85;
+      popups = 0.85;
     };
 
-    sansSerif = {
-      package = pkgs.dejavu_fonts;
-      name = "DejaVu Sans";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
+
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.nerd-fonts.caskaydia-cove;
+        name = "CaskaydiaCove Nerd Font";
+      };
+
+      emoji = {
+        package = pkgs.nerd-fonts.caskaydia-cove;
+        name = "CaskaydiaCove Nerd Font";
+      };
+
+      sizes = {
+        terminal = 13;
+      };
+
     };
 
-    monospace = {
-      package = pkgs.nerd-fonts.caskaydia-cove;
-      name = "CaskaydiaCove Nerd Font";
+    # Not Working
+    # iconTheme.enable = true;
+    # iconTheme.package = pkgs.vscode-extensions.tal7aouy.icons;
+
+    cursor = {
+      # package = pkgs.catppuccin-cursors.mochaLight;
+      # name = "catppuccin-mocha-light-cursors";
+
+      # package = pkgs.pokemon-cursor;
+      # name = "Pokemon";
+
+      package = pkgs.gruppled-black-cursors;
+      name = "gruppled_black";
+
+      size = 24;
     };
 
-    emoji = {
-      package = pkgs.nerd-fonts.caskaydia-cove;
-      name = "CaskaydiaCove Nerd Font";
+    targets.neovim = {
+      plugin = "base16-nvim";
+      transparentBackground.main = true;
+      transparentBackground.signColumn = true;
     };
+
+    wayland.windowManager.hyprland.settings.general."col.active_border" = lib.mkForce "rgba(${config.lib.stylix.colors.base0D}ff) rgba(${config.lib.stylix.colors.base08}ff) 45deg";
+    wayland.windowManager.hyprland.settings.general."col.inactive_border" = lib.mkForce "rgba(${config.lib.stylix.colors.base04}aa)";
+
   };
-
-  stylix.iconTheme.enable = true;
-
-  stylix.fonts.sizes.terminal = 13;
-
-  stylix.cursor = {
-    # package = pkgs.catppuccin-cursors.mochaLight;
-    # name = "catppuccin-mocha-light-cursors";
-
-    # package = pkgs.pokemon-cursor;
-    # name = "Pokemon";
-
-    package = pkgs.gruppled-black-cursors;
-    name = "gruppled_black";
-
-    size = 24;
-  };
-
-  stylix.targets.neovim = {
-    plugin = "base16-nvim";
-    transparentBackground.main = true;
-    transparentBackground.signColumn = true;
-  };
-
-
-  wayland.windowManager.hyprland.settings.general."col.active_border" = lib.mkForce "rgba(${config.lib.stylix.colors.base0D}ff) rgba(${config.lib.stylix.colors.base08}ff) 45deg";
-  # wayland.windowManager.hyprland.settings.general."col.active_border" = lib.mkForce "rgba(${config.lib.stylix.colors.base0D}ee) rgba(${config.lib.stylix.colors.base0A}ee) 45deg";
-
-  wayland.windowManager.hyprland.settings.general."col.inactive_border" = lib.mkForce "rgba(${config.lib.stylix.colors.base04}aa)";
 
 }
