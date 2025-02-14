076c09907563a22a0435b8a92eb9ec644c945441
kalthun <jameskwm@protonmail.com>
2025-02-13 20:55:03 -0500

    It's about time

--- flake.nix
+++ flake.nix
@@ -1,101 +1,118 @@
 {
-    description = "NixOS Configuration Flake";
+  description = "NixOS Configuration Flake";
 
-    inputs = {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
 
-        nixos-hardware.url = "github:nixos/nixos-hardware/master";
-
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        stylix.url = "github:danth/stylix";
-
-        nvf.url = "github:notashelf/nvf";
-
-        zjstatus.url = "github:dj95/zjstatus";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs = {
-        self,
-        nixpkgs,
-        nixos-hardware,
-        home-manager,
-        stylix,
-        nvf,
-        zjstatus,
-        ...
-    } @inputs:
-        # let
+    stylix.url = "github:danth/stylix";
 
-        # in
-    {
+    nvf.url = "github:notashelf/nvf";
 
-        # TODO: Less Repetition
-        # home-manager.nixosModules.home-manager = {
-        #     useGlobalPkgs = true;
-        #     useUserPackages = true;
-        #     backupFileExtension = "backup";
-        #     extraSpecialArgs = { inherit inputs; };
+    zjstatus.url = "github:dj95/zjstatus";
+  };
 
-        #     users = {
-        #       "kalthun" = import ./home.nix;
-        #     };
-        # };
+  outputs = {
+    self,
+    nixpkgs,
+    nixos-hardware,
+    home-manager,
+    stylix,
+    nvf,
+    zjstatus,
+    ...
+  } @ inputs:
+  # let
+  # in
+  {
+    # TODO: Less Repetition
+    # home-manager.nixosModules.home-manager = {
+    #     useGlobalPkgs = true;
+    #     useUserPackages = true;
+    #     backupFileExtension = "backup";
+    #     extraSpecialArgs = { inherit inputs; };
 
-        nixosConfigurations = {
+    #     users = {
+    #       "kalthun" = import ./home.nix;
+    #     };
+    # };
 
-            seed = nixpkgs.lib.nixosSystem {
-                specialArgs = { inherit inputs; };
-                modules = [
-                    ./hosts/seed/configuration.nix
-                    (import ./overlays)
-                    home-manager.nixosModules.home-manager {
-                        home-manager.useGlobalPkgs = true;
-                        home-manager.useUserPackages = true;
-                        home-manager.users.kalthun = import ./home.nix;
-                        home-manager.backupFileExtension = "backup";
-                        home-manager.extraSpecialArgs = { inherit inputs; };
-                    }
-                    stylix.nixosModules.stylix # <- Includes both NixOS and Homemanager Modules
-                ];
-            };
+    nixosConfigurations = {
 
-            runner = nixpkgs.lib.nixosSystem {
-                specialArgs = { inherit inputs; };
-                modules = [
-                    ./hosts/runner/configuration.nix
-                    (import ./overlays)
-                    home-manager.nixosModules.home-manager {
-                        home-manager.useGlobalPkgs = true;
-                        home-manager.useUserPackages = true;
-                        home-manager.users.kalthun = import ./home.nix;
-                        home-manager.backupFileExtension = "backup";
-                        home-manager.extraSpecialArgs = { inherit inputs; };
-                    }
-                    stylix.nixosModules.stylix # <- Includes both NixOS and Homemanager Modules
-                ];
-            };
+      seed = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/seed/configuration.nix
+          (import ./overlays)
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.kalthun = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = {inherit inputs;};
+          }
+          stylix.nixosModules.stylix # <- Includes both NixOS and Homemanager Modules
+        ];
+      };
 
-            pillar = nixpkgs.lib.nixosSystem {
-                specialArgs = { inherit inputs; };
-                modules = [
-                    ./hosts/pillar/configuration.nix
-                    (import ./overlays)
-                    home-manager.nixosModules.home-manager {
-                        home-manager.useGlobalPkgs = true;
-                        home-manager.useUserPackages = true;
-                        home-manager.users.kalthun = import ./home.nix;
-                        home-manager.backupFileExtension = "backup";
-                        home-manager.extraSpecialArgs = { inherit inputs; };
-                    }
-                    stylix.nixosModules.stylix # <- Includes both NixOS and Homemanager Modules
-                ];
-            };
+      runner = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/runner/configuration.nix
+          (import ./overlays)
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.kalthun = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = {inherit inputs;};
+          }
+          stylix.nixosModules.stylix # <- Includes both NixOS and Homemanager Modules
+        ];
+      };
 
-        };
+      pillar = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/pillar/configuration.nix
+          (import ./overlays)
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.kalthun = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = {inherit inputs;};
+          }
+          stylix.nixosModules.stylix # <- Includes both NixOS and Homemanager Modules
+        ];
+      };
+
+      traveller = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/traveller/configuration.nix
+          (import ./overlays)
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.kalthun = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = {inherit inputs;};
+          }
+          stylix.nixosModules.stylix # <- Includes both NixOS and Homemanager Modules
+        ];
+      };
+
     };
+  };
 }
