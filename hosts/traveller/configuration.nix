a4c9777b4054effdb90c1351a72837761515ff49
kalthun <jameskwm@protonmail.com>
2025-03-17 15:17:31 -0400

    good luck

--- hosts/traveller/configuration.nix
+++ hosts/traveller/configuration.nix
@@ -22,10 +22,11 @@
 
   # For Hybrid GPU
   hardware.nvidia.prime = {
-    offload = {
-      enable = true;
-      enableOffloadCmd = true;
-    };
+    # offload = {
+    #   enable = true;
+    #   enableOffloadCmd = true;
+    # };
+    sync.enable = true;
     intelBusId = "PCI:0:2:0";
     nvidiaBusId = "PCI:1:0:0";
   };
