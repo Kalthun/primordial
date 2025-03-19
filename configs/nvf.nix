5ea5c55e795013c1d0649b5a36cd66b0e759b441
kalthun <jameskwm@protonmail.com>
2025-03-18 22:41:06 -0400

    idk man idk

--- configs/nvf.nix
+++ configs/nvf.nix
@@ -36,29 +36,29 @@
         ];
 
         # Use Stylix Colors
-        theme = {
-          enable = true;
-          name = "base16";
-          base16-colors = {
-            base00 = config.lib.stylix.colors.base00;
-            base01 = config.lib.stylix.colors.base01;
-            base02 = config.lib.stylix.colors.base02;
-            base03 = config.lib.stylix.colors.base03;
-            base04 = config.lib.stylix.colors.base04;
-            base05 = config.lib.stylix.colors.base05;
-            base06 = config.lib.stylix.colors.base06;
-            base07 = config.lib.stylix.colors.base07;
-            base08 = config.lib.stylix.colors.base08;
-            base09 = config.lib.stylix.colors.base0E; # Swapped
-            base0A = config.lib.stylix.colors.base0A;
-            base0B = config.lib.stylix.colors.base0B;
-            base0C = config.lib.stylix.colors.base0C;
-            base0D = config.lib.stylix.colors.base0D;
-            base0E = config.lib.stylix.colors.base09; # Swapped
-            base0F = config.lib.stylix.colors.base0F;
-          };
-          transparent = true;
-        };
+        # theme = {
+        #   enable = true;
+        #   name = "base16";
+        #   base16-colors = {
+        #     base00 = config.lib.stylix.colors.base00;
+        #     base01 = config.lib.stylix.colors.base01;
+        #     base02 = config.lib.stylix.colors.base02;
+        #     base03 = config.lib.stylix.colors.base03;
+        #     base04 = config.lib.stylix.colors.base04;
+        #     base05 = config.lib.stylix.colors.base05;
+        #     base06 = config.lib.stylix.colors.base06;
+        #     base07 = config.lib.stylix.colors.base07;
+        #     base08 = config.lib.stylix.colors.base08;
+        #     base09 = config.lib.stylix.colors.base0E; # Swapped
+        #     base0A = config.lib.stylix.colors.base0A;
+        #     base0B = config.lib.stylix.colors.base0B;
+        #     base0C = config.lib.stylix.colors.base0C;
+        #     base0D = config.lib.stylix.colors.base0D;
+        #     base0E = config.lib.stylix.colors.base09; # Swapped
+        #     base0F = config.lib.stylix.colors.base0F;
+        #   };
+        #   transparent = true;
+        # };
 
         spellcheck.enable = true;
         autopairs.nvim-autopairs.enable = true;
