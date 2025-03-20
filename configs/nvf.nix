f8c6c583b8215c6b30c71a282039f9bff0ca9a8b
kalthun <jameskwm@protonmail.com>
2025-03-20 13:20:32 -0400

    updaing nv config

--- configs/nvf.nix
+++ configs/nvf.nix
@@ -35,31 +35,6 @@
           }
         ];
 
-        # Use Stylix Colors
-        # theme = {
-        #   enable = true;
-        #   name = "base16";
-        #   base16-colors = {
-        #     base00 = config.lib.stylix.colors.base00;
-        #     base01 = config.lib.stylix.colors.base01;
-        #     base02 = config.lib.stylix.colors.base02;
-        #     base03 = config.lib.stylix.colors.base03;
-        #     base04 = config.lib.stylix.colors.base04;
-        #     base05 = config.lib.stylix.colors.base05;
-        #     base06 = config.lib.stylix.colors.base06;
-        #     base07 = config.lib.stylix.colors.base07;
-        #     base08 = config.lib.stylix.colors.base08;
-        #     base09 = config.lib.stylix.colors.base0E; # Swapped
-        #     base0A = config.lib.stylix.colors.base0A;
-        #     base0B = config.lib.stylix.colors.base0B;
-        #     base0C = config.lib.stylix.colors.base0C;
-        #     base0D = config.lib.stylix.colors.base0D;
-        #     base0E = config.lib.stylix.colors.base09; # Swapped
-        #     base0F = config.lib.stylix.colors.base0F;
-        #   };
-        #   transparent = true;
-        # };
-
         spellcheck.enable = true;
         autopairs.nvim-autopairs.enable = true;
         snippets.luasnip.enable = true;
@@ -168,7 +143,6 @@
                 sha256 = "sha256-H1Nx0jgCKnaorjCNEUckJP2GOFmspcQTnfNSPPLxTM4=";
               };
             };
-            # Setup advanced logic
             setup = ''require("smart-tab").setup()'';
           };
 
