fe040d918e6d317c55f3882aa5caa60f799e7fc5
kalthun <jameskwmail@gmail.com>
2026-02-06 20:08:08 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -121,6 +121,23 @@
         ];
       };
 
+      root = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./hosts/root/configuration.nix
+          (import ./overlays)
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.kalthun = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
+            home-manager.extraSpecialArgs = {inherit inputs;};
+          }
+          stylix.nixosModules.stylix # <- Includes both NixOS and Homemanager Modules
+        ];
+      };
+
     };
   };
 }
